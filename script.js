edc47827d0aee04cc8c9862818d7c533f691a8b7
SentientToast1 <50114839+SentientToast1@users.noreply.github.com>
2023-12-06 21:47:41 +0530

    a

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 function buttonPress(){
     window.location.href = "wip.html";
 }
+
+var Rslider = document.getElementById('redSlider');
+var Gslider = document.getElementById('greenSlider');
+var Bslider = document.getElementById('blueSlider');
+var colorBox = document.getElementById('colorBox');
+var rv = document.getElementById('rv');
+var gv = document.getElementById('gv');
+var bv = document.getElementById('bv');
+
+function boxColorChanger(){
+    var Rvalue = Rslider.value;
+    var Gvalue = Gslider.value;
+    var Bvalue = Bslider.value;
+
+    colorBox.style.backgroundColor = `rgb(${Rvalue}, ${Gvalue}, ${Bvalue})`;
+    rv.textContent = Rvalue;
+    gv.textContent = Gvalue;
+    bv.textContent = Bvalue;
+}
+
+Rslider.addEventListener('input', boxColorChanger);
+Gslider.addEventListener('input', boxColorChanger);
+Bslider.addEventListener('input', boxColorChanger);
