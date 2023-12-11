a41c3a6ffd4c04861934b45ed38c57faa5ec6109
SentientToast1 <50114839+SentientToast1@users.noreply.github.com>
2023-12-11 14:09:07 +0000

    commit super

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
-var circText = "<p>Circles are fundamental geometric shapes defined by all points lying at an equal distance from a common center. In mathematics, circles play a crucial role, often represented by a single parameter—the radius. They exhibit symmetry and simplicity, making them essential in various mathematical disciplines such as geometry, trigonometry, and calculus.</p>";
-
+var circText = "<p>Circles are fundamental geometric shapes defined by all points lying at an equal distance from a common center. In mathematics, circles play a crucial role, often represented by a single parameter—the radius. They exhibit symmetry and simplicity, making them essential in various mathewmatical disciplines such as geometry, trigonometry, and calculus.</p>";
+var squareText = "<p>Squares are vital in math for simplicity and area calculations. In architecture, they offer stability and aesthetic appeal. They feature prominently in puzzles, digital imaging, computer science, and physics, showcasing their versatility and importance across various fields.</p>";
 //button js
 function buttonPress(){
     window.location.href = "wip.html";
@@ -29,7 +29,11 @@ function boxColorChanger(){
 Rslider.addEventListener('input', boxColorChanger);
 Gslider.addEventListener('input', boxColorChanger);
 Bslider.addEventListener('input', boxColorChanger);
+
+
 //circle js
+
+
 var modCirc = document.getElementById("circle");
 function modCircClick() {
     modCirc.classList.toggle("circClicked");
@@ -42,4 +46,19 @@ function modCircClick() {
     }
 }
 
+
+//Square js
+
+
+var modSquare = document.getElementById("square");
+function modSquareClick(){
+    modSquare.classList.toggle("squareClicked");
+    if(modSquare.classList.contains("squareClicked")){
+        modSquare.innerHTML = "";
+        setTimeout(() => {modSquare.innerHTML = squareText}, 300);
+    }
+    else {
+        modSquare.innerHTML= "<p>Squares!</p>";
+    }
+}
 //end of js
